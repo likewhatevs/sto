dd857f6bae6ccaf5aa302d2ba02dabcb5b0f7fd5
likewhatevs <12107998+likewhatevs@users.noreply.github.com>
2022-11-19 19:51:57 -0500

    init
    init

---
+++ src/parse.rs
@@ -0,0 +1,9 @@
+/*!
+I lifted all of this off of
+[this pdf](https://openlab-mu-internal.web.cern.ch/03_Documents/3_Technical_Documents/Technical_Reports/2011/Urs_Fassler_report.pdf)
+and [this crate](https://crates.io/crates/linux-perf-file-reader).
+
+If you are reading this author of these,
+
+Thank you so much!!!!!
+*/
