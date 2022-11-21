9bbcc6d6756e759abbb5a832e1e17f49e2d775f6
likewhatevs <patsomaru@icloud.com>
2022-11-21 04:25:09 -0500

    add a flamegraph of unmodified data, start adding rust stuff

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use binrw::io::BufReader;
-use binrw::BinRead;
 use std::fs::File;
 use std::io::{Cursor, Read};
+use std::sync::Arc;
+use tokio::time::{sleep, Duration};
+
+const TASK_COUNT: usize = 1000;
+const WORKER_COUNT: usize = 10;
+
+type TaskQueue = deadqueue::limited::Queue<usize>;
 
 
-fn main() -> Result<(), anyhow::Error> {
-    let path = "/Users/patsomaru/Documents/GitHub/sto/tests/perf.data";
+
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let path = "/Users/patsomaru/Documents/GitHub/sto/tests/cpu_data.txt";
     let file = File::open(path).expect("Cannot read file.");
+    let queue = Arc::new(TaskQueue::new(TASK_COUNT));
     let mut buf = BufReader::new(file);
-    let read = PerfFileHeader::read(&mut buf);
-    // let mut buf = BufReader::new(file);
-    // use nom::number::complete::be_u64;
+
+    let queue = queue.clone();
+    tokio::spawn(async move {
+        loop {
+            let task = queue.pop().await;
+            println!("worker[{}] processing task[{}] ...", worker, task);
+        }
+    });
 
     // just error out if flags are never 0.
 
