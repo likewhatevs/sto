ecaa87cccfd412213afe86c5e0b4e72b906d4c6c
likewhatevs <patsomaru@icloud.com>
2023-03-19 23:47:37 -0400

    update

--- migrations/20230305040305_init.down.sql
+++ migrations/20230305040305_init.down.sql
@@ -1,4 +1,6 @@
 -- Add down migration script here
+drop function findRegressions(start_time timestamp);
+drop function subtree(rootid bigint);
 drop index stack_node_data_symbol_file_line_number_idx;
 drop index stack_node_parent_id_id_idx;
 drop table stack_node;
